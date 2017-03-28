c1bfc35184e927eab51423b7533adaed9f4647cf
Andrea Pierleoni <andreap@ebi.ac.uk>
2017-03-28 13:42:26 +0100

    don't verify certs

--- app/__init__.py
+++ app/__init__.py
@@ -99,7 +99,7 @@ def create_app(config_name):
                        # sniffer_timeout=60
                        timeout=60 * 20,
                        maxsize=100,
-                       verify_certs=True,
+                       verify_certs=False,
                        connection_class=RequestsHttpConnection,
                        )
     '''elasticsearch handlers'''
