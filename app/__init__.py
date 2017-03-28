4001952fb3e9f6ee3a1ded8b5d6c290c411f9327
Andrea Pierleoni <andreap@ebi.ac.uk>
2017-03-28 12:30:15 +0100

    handle elasticsearch over https

--- app/__init__.py
+++ app/__init__.py
@@ -21,6 +21,7 @@ from app.common.signals import LogException
 from ipaddr import IPNetwork
 from mixpanel import Mixpanel
 from mixpanel_async import AsyncBufferedConsumer
+import certifi
 
 # from flask.ext.cors import CORS
 # from flask_limiter import Limiter
@@ -96,6 +97,7 @@ def create_app(config_name):
                        # sniffer_timeout=60
                        timeout=60 * 20,
                        maxsize=100,
+                       verify_certs=True,
                        )
     '''elasticsearch handlers'''
     app.extensions['esquery'] = esQuery(es,
